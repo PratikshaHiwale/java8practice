ec4e9a9fe2b5bdc70f182657503b8e75baa64eed
pratiksha <tech.pratiksha@gmail.com>
2023-12-15 23:07:23 +0530

    FilterandMapTest file is added

--- src/com/map/demo/MapAndFilterTest.java
+++ src/com/map/demo/MapAndFilterTest.java
@@ -1,11 +1,27 @@
 package com.map.demo;
 
-class Employee{
-	
+import java.util.Arrays;
+import java.util.List;
+
+class Employee {
+	int empNo;
+	String ename;
+	double salary;
+
+	public Employee(int empNo, String ename, double salary) {
+		super();
+		this.empNo = empNo;
+		this.ename = ename;
+		this.salary = salary;
+	}
+
 }
+
 public class MapAndFilterTest {
 	public static void main(String[] args) {
-		
+		List<Employee> employees = Arrays.asList(new Employee(102, "smith", 250000), new Employee(102, "john", 300000),
+				new Employee(103, "robert", 400000));
+		employees.stream().filter(employee->employee.salary<400000).map(employee-> employee.ename).forEach(System.out::println);
 	}
 
 }
