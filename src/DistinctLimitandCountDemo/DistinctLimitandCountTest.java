4635aa4cc2eb622025e15af4986882ff395d2b01
pratiksha <tech.pratiksha@gmail.com>
2023-12-16 17:05:06 +0530

    DistinctLimitandCount class file is modified

--- src/DistinctLimitandCountDemo/DistinctLimitandCountTest.java
+++ src/DistinctLimitandCountDemo/DistinctLimitandCountTest.java
@@ -2,11 +2,16 @@ package DistinctLimitandCountDemo;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class DistinctLimitandCountTest {
 public static void main(String[] args) {
 	List<String> vehicles=Arrays.asList("bus","car","bus","truck","car");
 	
 	vehicles.stream().distinct().forEach(System.out::println);
+	long noVehicles=vehicles.stream().distinct().count();
+	System.out.println(noVehicles);
+	vehicles.stream().limit(4).forEach(vehicle->System.out.println(vehicle));
+	
 }
 }
