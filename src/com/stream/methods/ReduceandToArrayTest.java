1574fe32d11fadddcbb89144e6df822b94b1b029
pratiksha <tech.pratiksha@gmail.com>
2023-12-19 23:25:12 +0530

    ReduceandToArray class file is modified

--- src/com/stream/methods/ReduceandToArrayTest.java
+++ src/com/stream/methods/ReduceandToArrayTest.java
@@ -7,10 +7,19 @@ import java.util.Optional;
 public class ReduceandToArrayTest {
 	public static void main(String[] args) {
 		List<String> stringList=Arrays.asList("A","B","C","1","2","3");
+		//reduce()
 		Optional<String> reduced=stringList.stream().reduce((value,combinedValue)->{
 			return combinedValue+value;
 		});
 		System.out.println(reduced.get());
+		//toArray
+		Object[] array=stringList.stream().toArray();
+		System.out.println(array.length);
+		 
+		for(Object obj:array) {
+			System.out.println(obj);
+		}
+		
 	}
-
+  
 }
